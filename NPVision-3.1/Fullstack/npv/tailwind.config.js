f176b86f2a7c0434bcb7a8732850f4c54ec821da
Sabir Ali Mondal <105653476+Sabir-Ali-Mondal@users.noreply.github.com>
2025-11-15 05:31:27 +0530

    Update tailwind.config.js

--- NPVision-3.1/Fullstack/npv/tailwind.config.js
+++ NPVision-3.1/Fullstack/npv/tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("tailwindcss-animate")
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
   ],
 };
