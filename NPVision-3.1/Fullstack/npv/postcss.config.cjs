67ad92b285d06ebfcca8267f334838364ed8b8a7
Sabir Ali Mondal <105653476+Sabir-Ali-Mondal@users.noreply.github.com>
2025-11-15 05:50:11 +0530

    Create postcss.config.cjs

---
+++ NPVision-3.1/Fullstack/npv/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // @tailwindcss/postcss plugin required for Tailwind v4 integration
+    "@tailwindcss/postcss": {}
+  }
+};
